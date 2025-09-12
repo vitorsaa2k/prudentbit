3ee106975e2bde86fa57aaac8e3cb891723287ab
vitorsaa2k <vitoradriel66@gmail.com>
2025-09-12 19:57:57 -0300

    fix deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Manrope } from "next/font/google";
 import "./globals.css";
 import { SearchProvider } from "@/context/SearchContext";
+import { Suspense } from "react";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -33,7 +34,9 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} ${manrope.className} antialiased`}
 			>
-				<SearchProvider>{children}</SearchProvider>
+				<Suspense>
+					<SearchProvider>{children}</SearchProvider>
+				</Suspense>
 			</body>
 		</html>
 	);
