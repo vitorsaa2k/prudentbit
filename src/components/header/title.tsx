a1fb45232208a6674b4267f05efa22106105f710
vitorsaa2k <vitoradriel66@gmail.com>
2025-09-12 19:53:29 -0300

    make title show the actual number of patients found

--- src/components/header/title.tsx
+++ src/components/header/title.tsx
@@ -1,8 +1,13 @@
+"use client";
+
+import { useSearch } from "@/context/SearchContext";
+
 export function HeaderTitle() {
+	const search = useSearch();
 	return (
 		<div className="ml-[41px] flex flex-col justify-center text-white">
 			<h1 className="font-bold text-[40px]">Patient Directory</h1>
-			<p className="text-[20px]">1000 Patient Found</p>
+			<p className="text-[20px]">{search.pagination?.total} Patient Found</p>
 		</div>
 	);
 }
