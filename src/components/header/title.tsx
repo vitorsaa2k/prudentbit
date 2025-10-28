df2cdf02cedce0fe6cf72d110eb2e13a0ce51a56
vitorsaa2k <vitoradriel66@gmail.com>
2025-10-28 14:41:57 -0300

    make patients found be "Patient" when only one patient is found

--- src/components/header/title.tsx
+++ src/components/header/title.tsx
@@ -4,10 +4,14 @@ import { useSearch } from "@/context/SearchContext";
 
 export function HeaderTitle() {
 	const search = useSearch();
+	const totalFound = search.pagination?.total ?? 0;
 	return (
 		<div className="ml-[41px] flex flex-col justify-center text-white">
 			<h1 className="font-bold text-[40px]">Patient Directory</h1>
-			<p className="text-[20px]">{search.pagination?.total} Patient Found</p>
+			<p className="text-[20px]">
+				{totalFound} Patient
+				{totalFound === 1 ? null : "s"} Found
+			</p>
 		</div>
 	);
 }
