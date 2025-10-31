bf12a077c7e4bed5d2116d6a0eaecbcdf05407ba
vitorsaa2k <vitoradriel66@gmail.com>
2025-10-31 17:33:10 -0300

    add local images

--- src/components/header/headerImg.tsx
+++ src/components/header/headerImg.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import headerImg from "../../../public/HeaderImgPrudenbit.png";
 
 export function HeaderImg() {
 	return (
@@ -7,7 +8,7 @@ export function HeaderImg() {
 			width={800}
 			height={200}
 			alt="header img"
-			src="https://cdn.discordapp.com/attachments/477882162834702348/1415476748136616047/image.png?ex=68c358f3&is=68c20773&hm=0f66519ad8e5a3f5fdbd5cf174fffdee179c2cdcdf2998148afa711e4ea133e9"
+			src={headerImg}
 		/>
 	);
 }
